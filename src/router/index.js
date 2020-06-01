7d6e103317d0982bccd92a39a3faea692dc33e5f
Chenyuxiang0123 <1013930927@qq.com>
2020-06-01 15:51:56 +0800

    构建项目 配置全局样式 引入elementui

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/login/index")
+  },
+  {
+    path: "/main",
+    name: "Main",
+    component: () => import("../views/main/index")
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    我是登录页
+    <router-link to="/main">首页</router-link>
+    <el-button type="primary">按钮</el-button>
+    <el-button type="warning">按钮</el-button>
+    <el-button type="danger">按钮</el-button>
+    <el-button type="success">按钮</el-button>
+    <el-button type="info">按钮</el-button>
+    <el-button>按钮</el-button>
+  </div>
+</template>
